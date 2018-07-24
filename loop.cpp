861233efb7a8c781897c8b79459698fe87dda185
Wasim <wasim_miet@yahoo.in>
2018-07-24 08:41:24 +0530

    Add files via upload
    Checks if the GPS logger is running. If not then start the GPS logger

---
+++ loop.cpp
@@ -0,0 +1,78 @@
+#include <sys/types.h>
+#include <dirent.h>
+#include <errno.h>
+#include <vector>
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+
+using namespace std;
+
+int getProcIdByName(string procName)
+{
+    int pid = -1;
+
+    // Open the /proc directory
+    DIR *dp = opendir("/proc");
+    if (dp != NULL)
+    {
+        // Enumerate all entries in directory until process found
+        struct dirent *dirp;
+        while (pid < 0 && (dirp = readdir(dp)))
+        {
+            // Skip non-numeric entries
+            int id = atoi(dirp->d_name);
+            if (id > 0)
+            {
+                // Read contents of virtual /proc/{pid}/cmdline file
+                string cmdPath = string("/proc/") + dirp->d_name + "/cmdline";
+                ifstream cmdFile(cmdPath.c_str());
+                string cmdLine;
+                getline(cmdFile, cmdLine);
+                if (!cmdLine.empty())
+                {
+                    // Keep first cmdline item which contains the program path
+                    size_t pos = cmdLine.find('\0');
+                    if (pos != string::npos)
+                        cmdLine = cmdLine.substr(0, pos);
+                    // Keep program name only, removing the path
+                    pos = cmdLine.rfind('/');
+                    if (pos != string::npos)
+                        cmdLine = cmdLine.substr(pos + 1);
+                    // Compare against requested process name
+                    if (procName == cmdLine)
+                        pid = id;
+                }
+            }
+        }
+    }
+
+    closedir(dp);
+
+    return pid;
+}
+
+int main(int argc, char* argv[])
+{
+    // Fancy command line processing skipped for brevity
+    int counter = 1;
+	while(1)
+	{
+	    int pid = getProcIdByName("gpsplus");
+	    cout << "pid: " << pid << endl;
+	    if (pid == -1)
+	    {
+		system("sudo ./gpsplus");
+		counter++;
+	    }
+	    else
+	    {
+		cout<<"----Code Counter:"<<counter<<endl;
+	    }
+	    sleep(1);
+	}
+    return 0;
+}
